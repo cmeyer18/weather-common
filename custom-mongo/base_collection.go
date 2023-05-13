478e09259943f1a0c3291cf945c54723a3973e81
Carson Meyer <carsondmeyer@gmail.com>
2023-05-12 23:32:59 -0500

    Add a BSON type getter

--- custom-mongo/base_collection.go
+++ custom-mongo/base_collection.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"reflect"
 )
 
 type BaseCollection[T any] struct {
@@ -15,8 +16,31 @@ func NewBaseCollection[T any](collection *mongo.Collection) BaseCollection[T] {
 	return BaseCollection[T]{collection: collection}
 }
 
+func (bc *BaseCollection[T]) getBSONType(fieldName string) string {
+	idField, ok := reflect.TypeOf((*T)(nil)).Elem().FieldByName(fieldName) // not json:name
+	if !ok {
+		return ""
+	}
+
+	idBsonTag := idField.Tag.Get("bson")
+	if idBsonTag != "" {
+		return ""
+	}
+
+	return idBsonTag
+}
+
 func (bc *BaseCollection[T]) Exists(field string, item interface{}) (bool, error) {
-	result := bc.collection.FindOne(context.TODO(), bson.M{field: item})
+	fieldBsonTag := bc.getBSONType(field)
+	if fieldBsonTag == "" {
+		return false, errors.New("bson type empty on field")
+	}
+
+	result := bc.collection.FindOne(context.TODO(), bson.M{fieldBsonTag: item})
+
+	if item == nil {
+		return false, errors.New("item is null")
+	}
 
 	var element T
 	err := result.Decode(&element)
@@ -45,7 +69,16 @@ func (bc *BaseCollection[T]) Insert(elements []T) error {
 }
 
 func (bc *BaseCollection[T]) Delete(field string, items []interface{}) error {
-	_, err := bc.collection.DeleteMany(context.TODO(), bson.M{field: bson.M{"$in": items}})
+	fieldBsonTag := bc.getBSONType(field)
+	if fieldBsonTag == "" {
+		return errors.New("bson type empty on field")
+	}
+
+	if items == nil || len(items) == 0 {
+		return errors.New("items is null")
+	}
+
+	_, err := bc.collection.DeleteMany(context.TODO(), bson.M{fieldBsonTag: bson.M{"$in": items}})
 	if err != nil {
 		return err
 	}
@@ -56,15 +89,17 @@ func (bc *BaseCollection[T]) Get(field string, items []interface{}) ([]T, error)
 	var elements []T
 	var results *mongo.Cursor
 
-	if field == "" {
-		return nil, errors.New("field should not be empty")
+	fieldBsonTag := bc.getBSONType(field)
+	if fieldBsonTag == "" {
+		return nil, errors.New("bson type empty on field")
 	}
+
 	if items == nil || len(items) == 0 {
 		return nil, errors.New("item should not be empty")
 	}
 
 	// Get all the records and process them into an array
-	results, err := bc.collection.Find(context.TODO(), bson.M{field: bson.M{"$in": items}})
+	results, err := bc.collection.Find(context.TODO(), bson.M{fieldBsonTag: bson.M{"$in": items}})
 	if err != nil {
 		return nil, err
 	}
