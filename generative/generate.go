601d6a987619f1a284b8c0560bd15078708aa453
cdmeyer <carsondmeyer@gmail.com>
2023-06-19 22:47:53 -0500

    Add generative code for enums that involve options

---
+++ generative/generate.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/cmeyer18/weather-common/generative/generators/golang"
+	"github.com/cmeyer18/weather-common/generative/generators/swift"
+	"os"
+)
+
+func main() {
+	print(os.Getwd())
+	golang.GenerateSpcOutlookGo()
+	golang.GenerateWeatherAlertsGo()
+	swift.GenerateSpcOutlookSwift()
+	swift.GenerateWeatherAlertsSwift()
+}
