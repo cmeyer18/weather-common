b6f87c9d413aa85ba8024758fa9400db866b3cbe
cdmeyer <carsondmeyer@gmail.com>
2023-06-21 23:43:09 -0500

    Add spc outlook collection

---
+++ custom_mongo/collections/spc_outlook_collection.go
@@ -0,0 +1,25 @@
+package collections
+
+import (
+	"github.com/cmeyer18/weather-common/custom_mongo"
+	"github.com/cmeyer18/weather-common/data_structures"
+)
+
+type SPCOutlookCollection struct {
+	*custom_mongo.BaseCollection[data_structures.SPCOutlook]
+}
+
+func NewSPCOutlookCollection(connection custom_mongo.BaseConnection) (SPCOutlookCollection, bool, bool, error) {
+	collection, dbExist, collectExist, err := custom_mongo.GetCollection[data_structures.SPCOutlook](spcOutlookCollectionArgs(), connection)
+	if err != nil {
+		return SPCOutlookCollection{}, false, false, err
+	}
+
+	collectionConv := SPCOutlookCollection{&collection}
+
+	return collectionConv, dbExist, collectExist, nil
+}
+
+func spcOutlookCollectionArgs() custom_mongo.BaseCollectionArgs {
+	return custom_mongo.BaseCollectionArgs{DatabaseName: "weather", CollectionName: "spcOutlook"}
+}
