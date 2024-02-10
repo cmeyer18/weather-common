b37140aa82ffb8c1825ffd96033987d64c4e15b1
Carson Meyer <carsondmeyer@gmail.com>
2024-02-09 20:31:55 -0600

    Add new field option

--- data_structures/user.go
+++ data_structures/user.go
@@ -6,16 +6,17 @@ import (
 )
 
 type UserNotification struct {
-	NotificationId           string                         `json:"notificationId"`
-	UserID                   string                         `json:"userid"`
-	ZoneCode                 string                         `json:"zonecode"`
-	CountyCode               string                         `json:"countycode"`
-	CreationTime             time.Time                      `json:"creationtime"`
-	Lat                      float64                        `json:"lat"`
-	Lng                      float64                        `json:"lng"`
-	FormattedAddress         string                         `json:"formattedaddress"`
-	APNKey                   string                         `json:"apnKey"`
-	LocationName             string                         `json:"locationName"`
-	ConvectiveOutlookOptions []golang.ConvectiveOutlookType `json:"convectiveOutlookOptions"`
-	AlertOptions             []golang.AlertType             `json:"alertOptions"`
+	NotificationId                   string                         `json:"notificationId"`
+	UserID                           string                         `json:"userid"`
+	ZoneCode                         string                         `json:"zonecode"`
+	CountyCode                       string                         `json:"countycode"`
+	CreationTime                     time.Time                      `json:"creationtime"`
+	Lat                              float64                        `json:"lat"`
+	Lng                              float64                        `json:"lng"`
+	FormattedAddress                 string                         `json:"formattedaddress"`
+	APNKey                           string                         `json:"apnKey"`
+	LocationName                     string                         `json:"locationName"`
+	ConvectiveOutlookOptions         []golang.ConvectiveOutlookType `json:"convectiveOutlookOptions"`
+	AlertOptions                     []golang.AlertType             `json:"alertOptions"`
+	MesoscaleDiscussionNotifications bool                           `json:"mesoscaleDiscussionNotifications"`
 }
