44c307e9bb5624351b285f21e543c82fbcdeb057
Carson Meyer <carsondmeyer@gmail.com>
2024-05-08 22:12:07 -0500

    Revert v1 changes

--- data_structures/mesoscale_discussion.go
+++ data_structures/mesoscale_discussion.go
@@ -3,7 +3,6 @@ package data_structures
 import "github.com/cmeyer18/weather-common/v4/data_structures/geojson"
 
 type MesoscaleDiscussion struct {
-	ID           string
 	MDNumber     int
 	Year         int
 	AffectedArea *geojson.Polygon
--- data_structures/server_notification_update.go
+++ data_structures/server_notification_update.go
@@ -1,21 +1,7 @@
 package data_structures
 
 type NotificationUpdate struct {
-	Alert               *Alert                `json:"alert,omitempty"`
-	ConvectiveOutlook   *ConvectiveOutlook    `json:"convectiveOutlook,omitempty"`
-	MesoscaleDiscussion *MesoscaleDiscussion  `json:"mesoscaleDiscussion,omitempty"`
+	Alert               *Alert               `json:"alert,omitempty"`
+	ConvectiveOutlook   *ConvectiveOutlook   `json:"convectiveOutlook,omitempty"`
+	MesoscaleDiscussion *MesoscaleDiscussion `json:"mesoscaleDiscussion,omitempty"`
 }
-
-type NotificationUpdateV2 struct {
-	Id string `json:"id"`
-	NotificationType NotificationType `json:"notificationType"`
-}
-
-type NotificationType string
-
-const (
-	AlertType NotificationType = "alert"
-	ConvectiveOutlookType NotificationType = "convectiveOutlook"
-	MesoscaleDiscussionType NotificationType = "mesoscaleDiscussion"
-)
-
