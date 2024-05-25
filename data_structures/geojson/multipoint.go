dea83faae972e757f1496e96f713374b579b66d5
Carson Meyer <carsondmeyer@gmail.com>
2024-05-25 09:57:40 -0500

    Remove deprecated code

--- data_structures/geojson/multipoint.go
+++
@@ -1,6 +0,0 @@
-package geojson
-
-// Deprecated: use MultiPointV2
-type MultiPoint struct {
-	Points []*Point `json:"points,omitempty"`
-}
--- data_structures/server_notification_update.go
+++
@@ -1,8 +0,0 @@
-package data_structures
-
-// Deprecated: use NotificationUpdateV2
-type NotificationUpdate struct {
-	Alert               *Alert               `json:"alert,omitempty"`
-	ConvectiveOutlook   *ConvectiveOutlook   `json:"convectiveOutlook,omitempty"`
-	MesoscaleDiscussion *MesoscaleDiscussion `json:"mesoscaleDiscussion,omitempty"`
-}
